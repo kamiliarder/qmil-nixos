bca7cdc0460ea5d174556b44f111188245249e97
kamiliarder <kamilworkmail19@gmail.com>
2026-09-13 15:27:29 +0800

    feat: spawn shell at startup

--- modules/home/qmil.nix
+++ modules/home/qmil.nix
@@ -5,6 +5,7 @@
       useUserPackages = true;
       users.qmil = {
         home.stateVersion = "26.05";
+	home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.myNoctalia ];
 
         xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
 
