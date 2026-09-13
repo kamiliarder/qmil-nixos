7b97b63580ab2fdfd6207d0acb1d599895e28572
kamiliarder <kamilworkmail19@gmail.com>
2026-09-13 11:00:18 +0800

    feat : add niri to spawn at startup and support for x11 apps

--- modules/features/niri.nix
+++ modules/features/niri.nix
@@ -10,11 +10,11 @@
     packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
       inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
       settings = {
-        # spawn-at-startup = [
-        #   (lib.getExe self'.packages.myNoctalia)
-        # ];
+        spawn-at-startup = [
+          (lib.getExe self'.packages.myNoctalia)
+        ];
 
-        # xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+        xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
 
         input.keyboard.xkb.layout = "us,ua";
 
@@ -23,7 +23,7 @@
         binds = {
           "Mod+T".spawn-sh = lib.getExe pkgs.foot;
           "Mod+Q".close-window = [];
-          # "Mod+S".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+          "Mod+Space".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
         };
       };
     };
