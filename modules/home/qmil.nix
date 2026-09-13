2c38be77000df513248e04716c85b81bd5a2e814
kamiliarder <kamilworkmail19@gmail.com>
2026-09-13 23:30:30 +0800

    feat: add direnv

--- modules/home/qmil.nix
+++ modules/home/qmil.nix
@@ -1,26 +1,40 @@
 { self, inputs, ... }: {
-  flake.nixosModules.qmilHome = { pkgs, ... }: {
+  flake.nixosModules.qmilHome = { config, pkgs, ... }: {
     home-manager = {
       useGlobalPkgs = true;
       useUserPackages = true;
-      users.qmil = {
+      users.qmil = { config, pkgs, ... }: {
         home.stateVersion = "26.05";
-	home.packages = [ self.packages.${pkgs.stdenv.hostPlatform.system}.myNoctalia ];
+
+        home.file.".config/nvim".source =
+          config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/modules/home/nvim";
+
+        home.packages = [
+          self.packages.${pkgs.stdenv.hostPlatform.system}.myNoctalia
+          pkgs.dbeaver-bin
+          pkgs.ripgrep
+          pkgs.fd
+          pkgs.unzip
+          pkgs.gcc
+          pkgs.gnumake
+          pkgs.python3
+        ];
 
         xdg.configFile."niri/config.kdl".source = ./niri-config.kdl;
         xdg.configFile."foot/foot.ini".source = ./foot.ini;
 
         programs.foot.enable = true;
-	programs.fish = {
-	   enable = true;
-	   shellAliases = {
-	      ll = "ls -la";
-	      cl = "clear";
-	   };
-	   shellInit= ''
-	      set -g fish_greeting ""
-	   '';
-	};
+        programs.fish = {
+          enable = true;
+          shellAliases = {
+            ll = "ls -la";
+            cl = "clear";
+          };
+          shellInit = ''
+            set -g fish_greeting ""
+            direnv hook fish | source
+          '';
+        };
       };
     };
     home-manager.backupFileExtension = "backup";
